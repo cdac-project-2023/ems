279ccd4a00705b9173ffd752c48e25d88b8901d0
dac-mar2023 <113843178+Mohit22456@users.noreply.github.com>
2023-08-29 10:51:03 +0530

    jwt error resolved

--- EMS/src/main/java/com/sunbeam/EmsApplication.java
+++ EMS/src/main/java/com/sunbeam/EmsApplication.java
@@ -1,13 +1,32 @@
 package com.sunbeam;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class EmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+//        System.out.println("In Model Mapper Bean");
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return mapper;
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
